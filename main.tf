e1e94a99b3e4977263d072f7d68e79cef5903edf
Shuntaro Nakano <nshun583@gmail.com>
2024-01-07 22:41:35 +0900

    modified tag location

--- main.tf
+++ main.tf
@@ -5,8 +5,7 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
-}
-
-tags = {
-    name = "terraform-example"
+    tags = {
+        name = "terraform-example"
+    }
 }
