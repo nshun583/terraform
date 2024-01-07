474283de06346ff9db066f4e1e84f76cd680f961
Shuntaro Nakano <nshun583@gmail.com>
2024-01-07 22:43:08 +0900

    name must be "Name"

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "example" {
     ami = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
     tags = {
-        name = "terraform-example"
+        Name = "terraform-example"
     }
 }
