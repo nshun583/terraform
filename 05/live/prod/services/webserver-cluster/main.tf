2690fe1c8104686fb1a34159ad058618ad9e6fc4
Shuntaro Nakano <nshun583@gmail.com>
2024-01-16 00:28:26 +0900

    implemented zero down time deploy

--- 05/live/prod/services/webserver-cluster/main.tf
+++ 05/live/prod/services/webserver-cluster/main.tf
@@ -5,6 +5,9 @@ provider "aws" {
 module "webserver-cluster" {
   source = "github.com/nshun583/terraform-modules//modules/services/webserver-cluster"
 
+  ami = "ami-0fb653ca2d3203ac1"
+  server_text = "New server text"
+
   cluster_name           = "webservers-prod"
   db_remote_state_bucket = "terraform-up-and-running-state-snakano"
   db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
--- 05/live/stage/services/webserver-cluster/main.tf
+++ 05/live/stage/services/webserver-cluster/main.tf
@@ -16,6 +16,9 @@ provider "aws" {
 module "webserver-cluster" {
   source = "github.com/nshun583/terraform-modules//modules/services/webserver-cluster"
 
+  ami = "ami-0fb653ca2d3203ac1"
+  server_text = "New server text"
+
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "terraform-up-and-running-state-snakano"
   db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
